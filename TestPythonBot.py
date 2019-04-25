a63667e0b60e7419af2c5bfb9584f95e938845c8
Rax Ixor <raxixor@gmail.com>
2019-04-26 00:23:52 +0300

    Working basic system. Kill command that overrides command handler

--- TestPythonBot.py
+++ TestPythonBot.py
@@ -1,4 +1,25 @@
-﻿import config
+﻿from config import CREDENTIALS
+import util
 import discord
 
-print(config.CREDENTIALS)
+class TestPythonBot(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as: {self.user}')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        
+        if util.check_kill_command(message.content) and util.is_owner(message.author.id):
+            await message.delete()
+            await self.logout()
+            if not self.is_closed():
+                await self.close()
+            print("Exited")
+            return
+
+        # TODO: Command Handler
+            
+
+client = TestPythonBot()
+client.run(CREDENTIALS["Token"])
---
+++ util.py
@@ -0,0 +1,15 @@
+﻿from config import CONFIGURATION
+
+def check_prefix(string: str):
+    return string.startswith(CONFIGURATION["Prefix"])
+
+def strip_prefix(string: str):
+    if string.startswith(CONFIGURATION["Prefix"]):
+        return string[len(CONFIGURATION["Prefix"]):]
+    return string
+
+def check_kill_command(string: str):
+    return string == CONFIGURATION["KillCommand"]
+
+def is_owner(id: int):
+    return id in CONFIGURATION["Owner"]
